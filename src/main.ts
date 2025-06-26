2141fe3ecdb8bfc5507efc2df59648ff43a4d8e8
Craig <cg37@outlook.com>
2025-06-26 19:40:11 +0800

    refactor: change main file path

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "@/style.scss";
+import App from "@/App.vue";
+import router from "@/router/index";
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
+createApp(App).use(pinia).use(router).mount("#app");
