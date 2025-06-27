32e73f7ec2d519b0f237409a8eaba53ac3be8e9a
Craig <cg37@outlook.com>
2025-06-27 20:01:02 +0800

    feat: new document

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { resolve } from "path";
 import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
   plugins: [vue(), svgLoader()],
+  base: "./",
+  build: {
+    outDir: "dist", // 输出目录
+    assetsDir: "./", // 静态资源目录
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html") // 确保正确解析路径
+      }
+    }
+  },
   server: {
     host: "0.0.0.0",
     port: 3031
   },
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    alias: { "@": resolve(__dirname, "./src") },
     extensions: [".js", ".ts", ".vue", ".json"] // 支持的文件扩展名
   },
   css: {
